29d34052f34cf39907af22a8afcf2515cc95b07f
ambulantelab <cdiazalv@calpoly.edu>
2025-01-28 08:31:19 -0800

    Removed the jetclocks JETGPIO and added new urdf file for the robot

---
+++ gobilda_ws/gobilda_ws/build/gobilda_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/gobilda/gobilda_ws/src/gobilda_robot
+# Build directory: /home/robot/gobilda/gobilda_ws/build/gobilda_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
